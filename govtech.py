fa02c6ad07402b67711ee2de00885b99cf3aecf3
ben <wangxuxin1@gmail.com>
2023-09-23 22:59:28 +0800

    successfully extracted information of all restaurants

--- govtech.py
+++ govtech.py
@@ -1,29 +1,72 @@
 import json
 import pandas as pd
 
-with open('restarant_data.json') as main_data:
-  main_data = main_data.read()
+# I want to store Restaurant Id
+# ◦   	Restaurant Name
+# ◦   	Country
+# ◦   	City
+# ◦   	User Rating Votes
+# ◦   	User Aggregate Rating (in float)
+# ◦   	Cuisines
+
+# I will have a list containing dictionaries of restaurants with a structure like this:
+
+# rest_id : {
+#     "name": "string",
+#     "country": "string",
+#     "city": "string",
+#     "user_rating_votes": "string",
+#     "user_aggregate_rating": "string",
+#     "cuisines": ["string", "string"]
+# }
 
 
-# df_json = pd.read_json('restaurant_data.json')
-# print(df_json)
+with open('restaurant_data.json', 'r') as file:
+    main_data = json.load(file)
 
+main_rest_lst = []
 
+for outer_dict in main_data:
+    
+    # Get the list of restaurants using outer_dict['restaurant']
+    rest_lst = outer_dict['restaurants']
 
+    # Loop through the list of restaurants, which themselves are dictionaries
+    for rest in range(len(rest_lst)):
+        rest_data = rest_lst[rest]['restaurant']
+        
+        # Initialise dict to store restaurant data
+        rest_dict = {}
 
-# Print out the keys and values
-# for key in parsed_json:
-#     print(key + ':', parsed_json[key])
+        # For Restaurant Id
+        rest_dict['Restaurant Id'] = rest_data.get('R', {}).get('res_id', 'NA')
 
-# Loop for rest_data keys and values
-# for key in rest_data:
-#     print(key + ':') 
-#     print("\n")
-#     print(rest_data[key])
-# # print(parsed_json)
+        # For Restaurant Name
+        rest_dict['Restaurant Name'] = rest_data.get('name', 'NA')
 
-# import json
+        # For User Rating Votes, User Aggregate Rating
+        rating_data = rest_data.get('user_rating', {})
+        rest_dict['User Rating Votes'] = rating_data.get('votes', 'NA')
+        rest_dict['User Aggregate Rating'] = float(rating_data.get('aggregate_rating', 'NA'))
 
-# with open('restaurant_data.json', 'r') as restaurant_data:
-#     restaurant_data = json.load(restaurant_data)
-#     print(json.dumps(restaurant_data, indent=4))
+        # For Country and City
+        location_data = rest_data.get('location', {})
+        rest_dict['Country'] = location_data.get('country_id', 'NA')
+        rest_dict['City'] = location_data.get('city', 'NA')
+
+        # For Cuisines
+        rest_dict['Cuisines'] = rest_data.get('cuisines', 'NA')
+
+        # Append the restaurant data to the main list
+        main_rest_lst.append(rest_dict)
+
+# Convert the list of dictionaries to a Pandas DataFrame
+df = pd.DataFrame(main_rest_lst)
+
+# Save the DataFrame to an Excel file
+file_name = "restaurants.xlsx"
+df.to_excel(file_name, index=False)
+
+print(f"Data saved to {file_name}")
+
+        
